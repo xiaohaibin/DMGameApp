c2125360724ac7784f1638eddf0b69dc5bdd9eba
Mr.xiao <xhb_199409@163.com>
2017-07-09 21:06:13 +0800

    设置腾讯id

--- app/src/main/java/com/stx/xhb/dmgameapp/base/BaseApplication.java
+++ app/src/main/java/com/stx/xhb/dmgameapp/base/BaseApplication.java
@@ -25,11 +25,12 @@ public class BaseApplication extends Application {
         //微信 appid appsecret
         PlatformConfig.setWeixin("wx152334f54a39c3b0", "24949aef9a179c253fdd55f12a576632");
         // QQ和Qzone appid appkey
-        PlatformConfig.setQQZone("100424468", "c7394704798a158208a74ab60104f0ba");
+        PlatformConfig.setQQZone("1105172050", "0QeUxsghHOHfZzap");
         //极光推送初始化
         JPushInterface.init(this);
         JPushInterface.setDebugMode(true);//设置是否开启log日志，正式打包发布时建议关闭使用
     }
+
     @Override
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
