ac16aa503576af95600c4845f9ff3e72b28e7f84
xiaohaibin <xiao.haibin05485@westone.com.cn>
2018-08-31 16:50:34 +0800

    代码重构

---
+++ app/src/main/java/com/stx/xhb/dmgameapp/data/remote/RemoteDataSource.java
@@ -0,0 +1,11 @@
+package com.stx.xhb.dmgameapp.data.remote;
+
+/**
+ * @author: xiaohaibin.
+ * @time: 2018/8/31
+ * @mail:xhb_199409@163.com
+ * @github:https://github.com/xiaohaibin
+ * @describe:
+ */
+public class RemoteDataSource {
+}
